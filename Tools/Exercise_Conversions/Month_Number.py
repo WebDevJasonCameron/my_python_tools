5b662aff0c6d941d375a871fa6c9315bd4d9cc0a
Jason Cameron <dev.jasoncameron@gmail.com>
2023-04-09 14:18:14 -0500

    build title comp

---
+++ Tools/Exercise_Conversions/Month_Number.py
@@ -0,0 +1,28 @@
+def get_month_number(month):
+
+    if month == "January":
+        return "01"
+    elif month == "February":
+        return "02"
+    elif month == "March":
+        return "03"
+    elif month == "April":
+        return "04"
+    elif month == "May":
+        return "05"
+    elif month == "June":
+        return "06"
+    elif month == "July":
+        return "07"
+    elif month == "August":
+        return "08"
+    elif month == "September":
+        return "09"
+    elif month == "October":
+        return "10"
+    elif month == "November":
+        return "11"
+    elif month == "December":
+        return "12"
+    else:
+        return "_Missing_"
--- Tools/Exercise_Conversions/Strong_App_Convert.py
+++ Tools/Exercise_Conversions/Strong_App_Convert.py
@@ -1,5 +1,37 @@
-from Strong_App_Convert import *
+import argparse
+import glob
 
-the_month = "February"
+from Month_Number import get_month_number
 
-print("the number for " + the_month + " is ")
+parser = argparse.ArgumentParser(
+    description='Remodelling the notion md files to go in a obsidian doc')
+
+parser.add_argument(
+    "--in_f", help="Targeted MD file used.", default="No Input Directory Arguments provided.")
+parser.add_argument(
+    "--out_p", help="The place the new file needs to go.", default="No Output Directory Arguments provided.")
+
+args = parser.parse_args()
+
+flag = True
+line_num = 0
+file_title = ""
+
+
+# (0 Day), (1 Month), (2 Day #), (3 Year), (4 "at"), (5 time), (6 "AM/PM")
+def build_file_title(line):
+    line_parts = line.replace(",", "").split(" ")
+    day = line_parts[0]
+    month_num_str = get_month_number(line_parts[1])
+    day_num_str = line_parts[2]
+    year_num_str = line_parts[3]
+    at = line_parts[4]
+    time = line_parts[5]
+    am_pm = line_parts[6]
+
+    return year_num_str + "_" + month_num_str + "_" + day_num_str + "__" + day + "__" + at + "_" + time + "_" + am_pm
+
+
+checks = build_file_title("Friday, December 10, 2021 at 9:41 AM")
+
+print(checks)
