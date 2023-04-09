da7c3f145ef6495aee7ac97d52d6dc72ff953638
Jason Cameron <dev.jasoncameron@gmail.com>
2023-04-09 13:36:45 -0500

    utils added

---
+++ LOG.md
@@ -0,0 +1,5 @@
+# Log
+
+## 2023 04 09
+
+Do I need a log for this repo? Probably not.
---
+++ Tools/Exercise_Conversions/Strong_App_Convert.py
@@ -0,0 +1,5 @@
+from Strong_App_Convert import *
+
+the_month = "February"
+
+print("the number for " + the_month + " is ")
