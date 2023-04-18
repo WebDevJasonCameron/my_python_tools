0f26b5546f1f9d6e59c6b1d894ec7fd6d5293c72
Jason Cameron <dev.jasoncameron@gmail.com>
2023-04-18 06:37:11 -0500

    add new tool, now we have mk table by row or col.  could combine and give user option with a param

---
+++ Tools/03_Make_Table/mk_table_by_row.py
@@ -0,0 +1,117 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+    description='Create markdown table from text and prompts.')
+
+parser.add_argument(
+    "--c", help="How many columns in the table.", default="No Arguments provided.")
+
+args = parser.parse_args()
+flag = True
+
+if args.c == "No column Arguments provided.":
+    flag = False
+
+
+def dash_counter(line):
+    output = ""
+    for d in line.rstrip():
+        output += "-"
+    return output
+
+
+def fill_lines():
+    read_file = open("input_text.txt", "r")
+    out_lines_list = []
+
+    for line in read_file:
+        if line == "\n":
+            continue
+        else:
+            out_lines_list.append(line.rstrip())
+
+    read_file.close()
+    return out_lines_list
+
+
+def build_table(lines, cols):
+    write_file = open("output_table.txt", "w")
+    rows = int(len(lines) / cols)
+    c_num = 1
+    r_num = 1
+    i_num = 0
+    n = 1
+
+    while n <= len(lines):
+
+        if r_num == 1 and c_num == 1:
+            write_file.writelines("| " + lines[i_num] + " | ")
+            i_num += 1
+            c_num += 1
+
+        elif r_num == 1 and c_num > 1 and c_num < cols:
+            write_file.writelines(lines[i_num] + " | ")
+            i_num += 1
+            c_num += 1
+
+        elif r_num == 1 and c_num == cols:
+            write_file.writelines(lines[i_num] + " |\n")
+            c_num = 1
+            r_num += 1
+            i_num += 1
+
+# 1ST ROW
+
+        elif r_num == 2 and c_num == 1:
+            write_file.writelines("| --- | ")
+            c_num += 1
+            n -= 1
+
+        elif r_num == 2 and c_num > 1 and c_num < cols:
+            write_file.writelines("--- | ")
+            c_num += 1
+            n -= 1
+
+        elif r_num == 2 and c_num == cols:
+            write_file.writelines("--- |\n")
+            c_num = 1
+            r_num += 1
+            n -= 1
+
+# 2ND ROW
+
+        elif r_num > 2 and c_num == 1:
+            write_file.writelines("| " + lines[i_num] + " | ")
+            i_num += 1
+            c_num += 1
+
+        elif r_num > 2 and c_num > 1 and c_num < cols:
+            write_file.writelines(lines[i_num] + " | ")
+            i_num += 1
+            c_num += 1
+
+        elif r_num > 2 and c_num > 1 and c_num == cols:
+            write_file.writelines(lines[i_num] + " |\n")
+            c_num = 1
+            r_num += 1
+            i_num += 1
+
+        else:
+            write_file.writelines("\nFound an Error\n")
+
+        n += 1
+    write_file.close()
+
+
+if flag:
+    print("Program is running...")
+
+    cols = int(args.c)
+
+    lines = fill_lines()
+    build_table(lines, cols)
+
+    for line in lines:
+        print(line)
+
+    print("Program complete.")
