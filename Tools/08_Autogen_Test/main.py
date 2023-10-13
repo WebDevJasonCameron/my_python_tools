1e19e54cfed7faf201fe51089b64b0ab2dbeadd9
Jason Cameron <dev.jasoncameron@gmail.com>
2023-10-13 13:20:51 -0500

    working ai agent tool

--- Tools/08_Autogen_Test/main.py
+++ Tools/08_Autogen_Test/main.py
@@ -1,10 +1,13 @@
 from keys.keys import provide_key1
 from autogen import AssistantAgent, UserProxyAgent, config_list_from_json
+import openai
 
-key = provide_key1()
+openai.api_key = provide_key1()
 
 config_list = config_list_from_json(env_or_file="OAI_CONFIG_LIST")
 assistant = AssistantAgent("assistant", llm_config={"config_list": config_list})
 user_proxy = UserProxyAgent("user_proxy", code_execution_config={"work_dir": "coding"})
-user_proxy.initiate_chat(assistant, message="Plot a chart of NVDA and TESLA stock price change YTD.")
+
+
+user_proxy.initiate_chat(assistant, message="build a simple snake game")
 
