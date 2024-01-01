8c82079b5f617b60e4e36cf8e5fafbb833fe58f9
Jason Cameron <dev.jasoncameron@gmail.com>
2024-01-01 08:07:35 -0600

    funct to capture atts and other arrays

--- Tools/08_Spell_Pages_to_SQL/Convert_MD_to_SQL/convert_md_to_sql.py
+++ Tools/08_Spell_Pages_to_SQL/Convert_MD_to_SQL/convert_md_to_sql.py
@@ -24,6 +24,7 @@ spell_classes = []
 attributes = []
 
 file_input = open("/Users/jasoncameron/Desktop/py_spells/Spells/acid arrow.md", "r");
+count_three_dashes = 0;
 
 
 # FUN
@@ -33,9 +34,55 @@ def Capture_Lines(input):
         if line.strip() == "":
             continue
         else:
-            lines.append(line.strip());
+            lines.append(line.strip().replace("\xa0", ""));
+
+# <F> Capture_Conditions
+
+# <F> Capture_Damagetypes
+
+# <F> Capture_Tags
+def Capture_Tags(lines):
+    for line in lines:
+        if line.startswith("spell_tags:"):
+            mod_line = line.replace("spell_tags:", "")
+            array_line = mod_line.split(",")
+            for l in array_line:
+                spell_tags.append(l.lower().strip())
+
+# <F> Capture_Classes
+def Capture_Classes(lines):
+    for line in lines:
+        if line.startswith("available_fore:"):
+            mod_line = line.replace("available_fore:", "")
+            array_line = mod_line.split(",")
+            for l in array_line:
+                spell_classes.append(l.lower().strip())
+
+# <F> Capture_Attributes
+def Capture_Attributes(lines):
+    count_bars = 1;
+    modline = ""
+
+    for line in lines:
+        if "|" in line and count_bars <= 2:
+            count_bars += 1
+        elif "|" in line and count_bars == 3:
+            mod_line = line.strip("| ")
+            array_line = mod_line.split("|")
+            for l in array_line:
+                attributes.append(l.lower().strip())
+        else:
+            continue
+
+
 
 
 
 # RUN ====================================================
-Capture_Lines(file_input);
+Capture_Lines(file_input)
+Capture_Tags(lines)
+Capture_Classes(lines)
+Capture_Attributes(lines)
+
+for line in attributes:
+    print(line)
