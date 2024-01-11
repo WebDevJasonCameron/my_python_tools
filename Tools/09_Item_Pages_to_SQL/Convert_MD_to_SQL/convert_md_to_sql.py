4732d8ff9caf35b8127e2b658260673472f7b75c
Jason Cameron <dev.jasoncameron@gmail.com>
2024-01-10 19:57:17 -0600

    continue parsing items mds

--- Tools/09_Item_Pages_to_SQL/Convert_MD_to_SQL/convert_md_to_sql.py
+++ Tools/09_Item_Pages_to_SQL/Convert_MD_to_SQL/convert_md_to_sql.py
@@ -10,7 +10,7 @@ item_id_number = 1
 # FINAL RUNNING FUNCTION
 
 # FUN
-# <F> Parse Document
+# <F> Parse_Document
 def Pars_Document(file):
     read_file = open(file, 'r')
 
@@ -18,11 +18,10 @@ def Pars_Document(file):
     item_tags = Capture_Tags(lines)
     item_types = Capture_Types(lines)
 
-    for type in item_types:
-        print(type)
+    Fill_In_Item(lines)
 
 
-# <F> Capture Lines
+# <F> Capture_Lines
 def Capture_Lines(input):
     lines = []
     for line in input:
@@ -33,7 +32,8 @@ def Capture_Lines(input):
 
     return lines
 
-# <F> Capture Types
+
+# <F> Capture_Types
 def Capture_Types(lines):
     item_types = []
 
@@ -44,7 +44,8 @@ def Capture_Types(lines):
 
     return item_types
 
-# <F> Capture Tags
+
+# <F> Capture_Tags
 def Capture_Tags(lines):
     item_tags = []
 
@@ -57,26 +58,25 @@ def Capture_Tags(lines):
 
     return item_tags
 
-# <F> Capture Notes
 
-# <F> Capture Conditions
+# <F> Capture_Conditions
 def Capture_Condition(lines, item_description):
     item_conditions = []
-    condition_list = [ "blinded",
-                        "charmed",
-                        "deafened",
-                        "exhaustion",
-                        "frightened",
-                        "grappled",
-                        "incapacitated",
-                        "invisible",
-                        "paralyzed",
-                        "petrified",
-                        "poisoned",
-                        "prone",
-                        "restrained",
-                        "stunned",
-                        "unconscious"]
+    condition_list = ["blinded",
+                      "charmed",
+                      "deafened",
+                      "exhaustion",
+                      "frightened",
+                      "grappled",
+                      "incapacitated",
+                      "invisible",
+                      "paralyzed",
+                      "petrified",
+                      "poisoned",
+                      "prone",
+                      "restrained",
+                      "stunned",
+                      "unconscious"]
 
     mod_description = item_description.lower().replace("[", " ").replace("]", " ").replace(",", "").replace(".", "")
     array_description = mod_description.split(" ")
@@ -87,13 +87,71 @@ def Capture_Condition(lines, item_description):
 
     return item_conditions
 
-# <F> Capture Attached Spells
+
+# <F> Capture_Attached_Spells
+
 
 # <F> Capture Effects
 
+# <F> Fill_In_Item
+def Fill_In_Item(lines):
+    item = {"name": "",                                             # 1.
+            "ttrpg": "DND5E",                                       # 2.
+            "rarity": "",                                           # 3.
+            "renowned_quality": "",                                 # 4.
+            "requires_attunement": False,                           # 5.
+            "has_charges": False,                                   # 6. Search Description
+            "is_cursed": False,                                     # 7.
+            "cost": "",                                             # 8. Blank
+            "weight": "",                                           # 9. Blank
+            "description": "",                                      # 10.
+            "image_url": "",                                        # 11. Blank
+            "source_id": 5,                                         # 12.
+            "magic_bonus_plus_1": False,                            # 13. Search Description
+            "magic_bonus_plus_2": False,                            # 14. Search Description
+            "magic_bonus_plus_3": False,                            # 15. Search Description
+            "description_notes": ""                                 # 16.
+            }
+    count_three_dashes = 0
+    string_description = ""
+
+    for line in lines:
+        if line.startswith("# "):
+            item["name"] = line.replace("#", "").replace("'", "''").strip()
+        elif line.startswith("rarity:"):
+            item["rarity"] = line.replace("rarity:", "").replace("'", "''").strip()
+        elif line.startswith("renowned_quality:"):
+            item["renowned_quality"] = line.replace("renowned_quality:", "").replace("'", "''").strip()
+        elif line.startswith("req_attunement:"):
+            if "yes" in line.lower():
+                item["requires_attunement"] = True
+        elif line.startswith("is_cursed:"):
+            if "yes" in line.lower():
+                item["is_cursed"] = True
+        elif line.startswith("Notes:"):
+            item["description_notes"] = line.replace("Notes:", "").replace("'", "''").strip()
+        elif line.startswith("!["):
+            continue
+        elif "---" in line and count_three_dashes <= 3:
+            count_three_dashes += 1
+            print("count: " + str(count_three_dashes))
+        elif count_three_dashes >= 3:
+            print("HERE!")
+            string_description += line.replace("'", "''").replace("**", "").replace("_", "") + "\n"
+
+    item["description"] = string_description
 
 
+    print("1. name: " + item["name"])
+    print("2. ttrpg: " + item["ttrpg"])
+    print("3. rarity: " + item["rarity"])
+    print("4. renowned quality: " + item["renowned_quality"])
+    print("5. requires attunement: " + str(item["requires_attunement"]))
+    print("6. has charges: " + str(item["has_charges"]))                            # <R> Req Description
+    print("7. is cursed: " + str(item["is_cursed"]))
+    print("8. cost: " + str(item["cost"]))
+    print("9. weight: " + str(item["weight"]))
+    print("9. description: " + str(item["description"]))
 
 # RUN ====================================================
 Pars_Document(file)
-
